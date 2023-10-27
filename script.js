395324ad79dfcc15a10e14e55daed492bfbf1529
Alex Moran <ebs.alex@icloud.com>
2023-10-27 00:40:00 -0700

    added function declarations: cpuChoice, playerChoice, playRound

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("yo")
+
+
+let options = ["Rock","Paper","Scissors"]
+
+let player = playerChoice()
+let cpu = cpuChoice()
+playRound(player, cpu)
+
+
+
+
+///function declarations
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+  }
+
+function cpuChoice() {
+    let index = random(options.length);
+    let cpuMove = options[index];
+    // alert(cpuMove)
+    return cpuMove;
+}
+
+function playerChoice() {
+    return prompt("Rock, Paper or Scissors...","")
+}
+
+function playRound(player, cpu) {
+    alert(player+" "+cpu)
+}
