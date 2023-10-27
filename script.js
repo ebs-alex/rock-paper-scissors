7dbad0d3962df26fa6d0153ba1b87cb915006fcb
Alex Moran <ebs.alex@icloud.com>
2023-10-27 10:19:53 -0700

    added final function declarations: displayScore, assignWin and main game loop with score assesor.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 
-let options = ["Rock","Paper","Scissors"]
+let options = ["rock","paper","scissors"]
+let playerScore = 0
+let cpuScore = 0
 
-let player = playerChoice()
-let cpu = cpuChoice()
-playRound(player, cpu)
+let amountToWin = parseInt(prompt("first to how many wins?", "3"))
 
+game()
 
 
 
@@ -23,9 +24,84 @@ function cpuChoice() {
 }
 
 function playerChoice() {
-    return prompt("Rock, Paper or Scissors...","")
+    return prompt("Rock, Paper or Scissors...","").toLowerCase()
 }
 
 function playRound(player, cpu) {
-    alert(player+" "+cpu)
+    alert(`You chose: ${player},\nCPU chose: ${cpu}`)
+    if (player == "rock") {
+        switch (cpu) {
+            case "rock":
+                assignWin("tie")
+                break;
+            case "paper":
+                assignWin("cpu")
+                break;
+            case "scissors":
+                assignWin("player")
+                break;
+        }
+    } else if (player == "paper") {
+        switch (cpu) {
+            case "rock":
+                assignWin("player")
+                break;
+            case "paper":
+                assignWin("tie")
+                break;
+            case "scissors":
+                assignWin("cpu")
+                break;
+        }
+    } else if (player == "scissors") {
+        switch (cpu) {
+            case "rock":
+                assignWin("cpu")
+                break;
+            case "paper":
+                assignWin("player")
+                break;
+            case "scissors":
+                assignWin("tie")
+                break;
+        }
+    } else {
+        alert("CANCELLED")
+    }
+}
+
+function assignWin(winner) {
+    if (winner != "tie") {
+        alert(`${winner} wins`)
+        if (winner == "player") {
+            ++playerScore
+            displayScore()
+        } else {
+            ++cpuScore
+            displayScore()
+        }
+    } else {
+        alert("Tie")
+        displayScore()
+    }
+}
+
+function displayScore () {
+    alert(`Player Score: ${playerScore}, \nCPU Score: ${cpuScore}`)
+}
+
+function game() {
+    while (playerScore < amountToWin && cpuScore < amountToWin) {
+        let player = playerChoice()
+        let cpu = cpuChoice()
+        playRound(player, cpu)
+        console.log(playerScore)
+    }
+
+    if (playerScore == amountToWin) {
+        alert("Player wins the game!");
+    } else {
+        alert("CPU wins the game!");
+    }
+
 }
