21e32e664486019053decef35e0796422af846a8
Alex Moran <ebs.alex@icloud.com>
2023-11-02 16:29:28 -0700

    UI features complete

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
 
+let amountToWin = parseInt(prompt("first to how many wins?", "3"))
 
-let options = ["rock","paper","scissors"]
+
+const score = document.querySelector('#score');
 let playerScore = 0
 let cpuScore = 0
 
-let amountToWin = parseInt(prompt("first to how many wins?", "3"))
+let options = ["Rock","Paper","Scissors"]
+const selections = document.querySelectorAll(".selection")
+selections.forEach( selection => {
+    selection.addEventListener('click', () => setPlayerSelection(selection.textContent))
+})
+const playerSelection = document.querySelector("#playerSelection")
+const cpuSelection = document.querySelector("#cpuSelection")
 
-game()
+const result = document.querySelector("#result")
 
 
 
-///function declarations
+
+
+// game()
+
+
+
+    ///function declarations
 
 function random(number) {
     return Math.floor(Math.random() * number);
   }
 
-function cpuChoice() {
+function setCpuChoice() {
     let index = random(options.length);
     let cpuMove = options[index];
-    // alert(cpuMove)
-    return cpuMove;
+    cpuSelection.textContent = `CPU chose ... ${cpuMove}`
+    return cpuMove
 }
 
-function playerChoice() {
-    return prompt("Rock, Paper or Scissors...","").toLowerCase()
+function setPlayerSelection(selection) {
+    playerSelection.textContent = `You chose ... ${selection}`
+    let cpu = setCpuChoice()
+    assessRound(selection, cpu)
 }
 
-function playRound(player, cpu) {
-    alert(`You chose: ${player},\nCPU chose: ${cpu}`)
-    if (player == "rock") {
+function assessRound(player, cpu) {
+    // alert(`You chose: ${player},\nCPU chose: ${cpu}`)
+    if (player == "Rock") {
         switch (cpu) {
-            case "rock":
+            case "Rock":
                 assignWin("tie")
                 break;
-            case "paper":
-                assignWin("cpu")
+            case "Paper":
+                assignWin("CPU")
                 break;
-            case "scissors":
-                assignWin("player")
+            case "Scissors":
+                assignWin("PLAYER")
                 break;
         }
-    } else if (player == "paper") {
+    } else if (player == "Paper") {
         switch (cpu) {
-            case "rock":
-                assignWin("player")
+            case "Rock":
+                assignWin("PLAYER")
                 break;
-            case "paper":
+            case "Paper":
                 assignWin("tie")
                 break;
-            case "scissors":
-                assignWin("cpu")
+            case "Scissors":
+                assignWin("CPU")
                 break;
         }
-    } else if (player == "scissors") {
+    } else if (player == "Scissors") {
         switch (cpu) {
-            case "rock":
-                assignWin("cpu")
+            case "Rock":
+                assignWin("CPU")
                 break;
-            case "paper":
-                assignWin("player")
+            case "Paper":
+                assignWin("PLAYER")
                 break;
-            case "scissors":
+            case "Scissors":
                 assignWin("tie")
                 break;
         }
@@ -72,36 +88,47 @@ function playRound(player, cpu) {
 
 function assignWin(winner) {
     if (winner != "tie") {
-        alert(`${winner} wins`)
-        if (winner == "player") {
+        result.textContent = `${winner} wins`
+        if (winner == "PLAYER") {
             ++playerScore
             displayScore()
+            assessGame()
+            result.style.color = "lightgreen"
         } else {
             ++cpuScore
             displayScore()
+            assessGame()
+            result.style.color = "red"
         }
     } else {
-        alert("Tie")
+        result.textContent = "Tie"
+        result.style.color = "white"
         displayScore()
+        assessGame()
     }
 }
 
 function displayScore () {
-    alert(`Player Score: ${playerScore}, \nCPU Score: ${cpuScore}`)
+    score.innerHTML = `Player Score: ${playerScore}<br>CPU Score: ${cpuScore}`
 }
 
-function game() {
-    while (playerScore < amountToWin && cpuScore < amountToWin) {
-        let player = playerChoice()
-        let cpu = cpuChoice()
-        playRound(player, cpu)
-        console.log(playerScore)
-    }
-
+function assessGame() {
     if (playerScore == amountToWin) {
         alert("Player wins the game!");
-    } else {
+        resetGame()
+    } else if (cpuScore == amountToWin) {
         alert("CPU wins the game!");
+        resetGame()
     }
 
+}
+
+function resetGame() {
+    playerScore = 0
+    cpuScore = 0
+    score.innerHTML = ''
+    result.textContent = ''
+    playerSelection.textContent = ''
+    cpuSelection.textContent = ''
+    result.style.color = "white"
 }
